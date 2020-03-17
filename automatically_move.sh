49471b874ec192f2863417e0543084e559af8fab
bigwheel <k.bigwheel+eng@gmail.com>
2020-03-17 19:36:33 +0900

    Fix automatically_move.sh

--- automatically_move.sh
+++ automatically_move.sh
@@ -7,9 +7,9 @@ similarity_threshold=${1:-1.0}
 plan_result_temp=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
 plan_result_json_temp=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
 
-# terraform plan -out=$plan_result_temp > /dev/null 2>&1
-# terraform show -json $plan_result_temp > $plan_result_json_temp 2> /dev/null
-# infermv $plan_result_json_temp $similarity_threshold | awk '{print "terraform state mv", $1, $2}'
+terraform plan -out=$plan_result_temp > /dev/null 2>&1
+terraform show -json $plan_result_temp > $plan_result_json_temp 2> /dev/null
+
 while read -u 3 line; do
   echo $line
 
@@ -18,5 +18,4 @@ while read -u 3 line; do
     [yY]*) echo $line | awk '{print "terraform state mv", $1, $2}';;
     *) echo "skip";;
   esac
-done 3< <(echo 'abc	def
-ghi	jkl')
+done 3< <(infermv $plan_result_json_temp $similarity_threshold)
