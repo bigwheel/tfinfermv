cca1c175ec1dac6a57ed047a7584ae61afa3f7ae
bigwheel <k.bigwheel+eng@gmail.com>
2020-03-17 19:32:06 +0900

    [In progress] Add automatically_move.sh

---
+++ automatically_move.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+similarity_threshold=${1:-1.0}
+
+plan_result_temp=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
+plan_result_json_temp=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
+
+# terraform plan -out=$plan_result_temp > /dev/null 2>&1
+# terraform show -json $plan_result_temp > $plan_result_json_temp 2> /dev/null
+# infermv $plan_result_json_temp $similarity_threshold | awk '{print "terraform state mv", $1, $2}'
+while read -u 3 line; do
+  echo $line
+
+  read -p "move? (y/N): " yn
+  case "$yn" in
+    [yY]*) echo $line | awk '{print "terraform state mv", $1, $2}';;
+    *) echo "skip";;
+  esac
+done 3< <(echo 'abc	def
+ghi	jkl')
