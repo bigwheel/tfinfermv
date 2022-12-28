19110065c68496d4447758f939951d5513b7f0d6
k.bigwheel <k.bigwheel+eng@gmail.com>
2022-12-28 17:08:12 +0900

    movedブロック対応で変更

--- generate_state_mv.sh
+++ generate_state_mv.sh
@@ -9,4 +9,4 @@ plan_result_json_temp=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
 
 terraform plan -out=$plan_result_temp > /dev/null 2>&1
 terraform show -json $plan_result_temp > $plan_result_json_temp 2> /dev/null
-infermv $plan_result_json_temp $similarity_threshold | awk '{print "terraform state mv", $1, $2}'
+infermv $plan_result_json_temp $similarity_threshold
