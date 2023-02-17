f4b4d201cd7a6ac2589d81da845c8811d5e336eb
k.bigwheel (kazufumi nishida) <k.bigwheel+eng@gmail.com>
2023-02-17 14:59:07 +0900

    Update generate_state_mv.sh

--- generate_state_mv.sh
+++ generate_state_mv.sh
@@ -7,6 +7,6 @@ similarity_threshold=${1:-1.0}
 plan_result_temp=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
 plan_result_json_temp=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
 
-terraform plan -out=$plan_result_temp > /dev/null 2>&1
-terraform show -json $plan_result_temp > $plan_result_json_temp 2> /dev/null
+terraform plan -out=$plan_result_temp 1>&2
+terraform show -json $plan_result_temp > $plan_result_json_temp
 infermv $plan_result_json_temp $similarity_threshold
